24e53f4aef910f73496fb9eb1c4e1758609c7d67
dicetherdev <dev@dicether.com>
2018-02-26 12:22:43 +0100

    Change network type test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ const ConflictResolution = artifacts.require("./ConflictResolution.sol");
 module.exports = function(deployer, network, accounts) {
     let serverAccount = "0x73fd0d10438be13a31ff12e20790f2002cdb8529";
     let houseAccount = "0xb9629e0766491dfdb94f632741da28945d4c03ce";
-    if (network != "rinkeby") {
+    if (network === "development") {
         serverAccount = accounts[1];
         houseAccount = accounts[4];
     }
