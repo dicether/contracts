173b14e19e3a8e2aefb36e52320f227c9a395240
dicetherdev <dev@dicether.com>
2018-05-15 13:20:18 +0200

    Fix tests: adjust stake, betvalue and timeout

--- test/utils/stateChannel.ts
+++ test/utils/stateChannel.ts
@@ -32,15 +32,15 @@ export enum GameType {
 
 export const HOUSE_STAKE = new BigNumber('10e18');
 export const MIN_STAKE = new BigNumber('1e16');
-export const MAX_STAKE = new BigNumber('2e17');
+export const MAX_STAKE = new BigNumber('5e17');
 export const MIN_VALUE = new BigNumber('1e13');
-export const MAX_VALUE = new BigNumber('1e16');
+export const MAX_VALUE = new BigNumber('2e16');
 export const MAX_BALANCE = MAX_VALUE.mul(500);
 
 export const NOT_ENDED_FINE = new BigNumber('1e15');
 
-export const SERVER_TIMEOUT = 2 * 24 * 3600;
-export const PLAYER_TIMEOUT = 2 * 24 * 3600;
+export const SERVER_TIMEOUT = 6 * 3600;
+export const PLAYER_TIMEOUT = 6 * 3600;
 
 export const PROFIT_TRANSFER_TIMESPAN_MIN = 24 * 60 * 60;
 export const PROFIT_TRANSFER_TIMESPAN_MAX = 6 * 30 * 24 * 60 * 60;
