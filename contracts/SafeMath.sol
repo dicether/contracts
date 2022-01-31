4008cf5c07e6b8591e79f0b74f68cbc97fd60738
dicetherdev <dev@dicether.com>
2022-01-31 23:16:22 +0100

    Update SafeMath to solidity 0.8

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,10 +1,11 @@
-// SPDX-License-Identifier: AGPL-3.0-only
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
 
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error.
+ * No checks necessary for solidity >= 0.8.
  * From zeppelin-solidity
  */
 library SafeMath {
@@ -13,40 +14,20 @@ library SafeMath {
     * @dev Multiplies two unsigned integers, throws on overflow.
     */
     function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-        if (a == 0) {
-            return 0;
-        }
-
-        c = a * b;
-        assert(c / a == b);
-        return c;
+        return a * b;
     }
 
     /**
     * @dev Multiplies two signed integers, throws on overflow.
     */
     function mul(int256 a, int256 b) internal pure returns (int256) {
-        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-        if (a == 0) {
-            return 0;
-        }
-        int256 c = a * b;
-        assert(c / a == b);
-        return c;
+        return a * b;
     }
 
     /**
     * @dev Integer division of two unsigned integers, truncating the quotient.
     */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        // uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return a / b;
     }
 
@@ -54,10 +35,6 @@ library SafeMath {
     * @dev Integer division of two signed integers, truncating the quotient.
     */
     function div(int256 a, int256 b) internal pure returns (int256) {
-        // assert(b > 0); // Solidity automatically throws when dividing by 0
-        // Overflow only happens when the smallest negative int is multiplied by -1.
-        int256 INT256_MIN = int256((uint256(1) << 255));
-        assert(a != INT256_MIN || b != - 1);
         return a / b;
     }
 
@@ -65,7 +42,6 @@ library SafeMath {
     * @dev Subtracts two unsigned integers, throws on overflow (i.e. if subtrahend is greater than minuend).
     */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        assert(b <= a);
         return a - b;
     }
 
@@ -73,26 +49,20 @@ library SafeMath {
     * @dev Subtracts two signed integers, throws on overflow.
     */
     function sub(int256 a, int256 b) internal pure returns (int256) {
-        int256 c = a - b;
-        assert((b >= 0 && c <= a) || (b < 0 && c > a));
-        return c;
+        return a - b;
     }
 
     /**
     * @dev Adds two unsigned integers, throws on overflow.
     */
     function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-        c = a + b;
-        assert(c >= a);
-        return c;
+        return a + b;
     }
 
     /**
     * @dev Adds two signed integers, throws on overflow.
     */
     function add(int256 a, int256 b) internal pure returns (int256) {
-        int256 c = a + b;
-        assert((b >= 0 && c >= a) || (b < 0 && c < a));
-        return c;
+        return a + b;
     }
 }
