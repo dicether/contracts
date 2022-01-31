6b4922f12c986e8598a1136ad2a4e9b476a64376
dicetherdev <dev@dicether.com>
2022-01-31 19:02:50 +0100

    Use -MAX_INT in abs test

--- test/mathUtil.ts
+++ test/mathUtil.ts
@@ -9,7 +9,7 @@ configureChai();
 const expect = chai.expect;
 
 contract("MathUtilMock", () => {
-    const MIN_INT = new BN(2).pow(new BN(255)).neg();
+    const MAX_INT = new BN(2).pow(new BN(255)).subn(1);
     let mathUtil: any;
 
     before(async () => {
@@ -43,9 +43,9 @@ contract("MathUtilMock", () => {
             expect(res).to.eq.BN(Math.abs(a));
         });
 
-        it("Should work for MIN_INT", async () => {
-            const res = await mathUtil.abs(MIN_INT);
-            expect(res).to.eq.BN(MIN_INT.abs());
+        it("Should work for -MAX_INT", async () => {
+            const res = await mathUtil.abs(MAX_INT.neg());
+            expect(res).to.eq.BN(MAX_INT);
         });
     });
 });
